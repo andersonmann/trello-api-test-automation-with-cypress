f294ac30ea812ef34adade8ced0f483ff03caa83
andersonmann <anderson.civil@hotmail.com>
2025-05-29 03:01:49 -0300

    adicionado novos metodos

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,11 @@
+const baseURL = 'https://api.trello.com/1/boards/';
 Cypress.Commands.add('createTrelloBoard', (name) => {
   const apiKey = Cypress.env('trelloApiKey');
   const apiToken = Cypress.env('trelloApiToken');
 
   return cy.request({
     method: 'POST',
-    url: 'https://api.trello.com/1/boards/',
+    url: baseURL,
     qs: { name, key: apiKey, token: apiToken }
   });
 });
@@ -17,5 +18,43 @@ Cypress.Commands.add('deleteTrelloBoard', (boardId) => {
     method: 'DELETE',
     url: `https://api.trello.com/1/boards/${boardId}`,
     qs: { key: apiKey, token: apiToken }
+  });
+});
+
+Cypress.Commands.add('getListsFromBoard', (boardId) => {
+  const apiKey = Cypress.env('trelloApiKey');
+  const apiToken = Cypress.env('trelloApiToken');
+  return cy.request({
+    method: 'GET',
+    url: `https://api.trello.com/1/boards/${boardId}/lists`,
+    qs: { key: apiKey, token: apiToken }
+  });
+});
+
+Cypress.Commands.add('createTrelloCard', ({ name, idList }) => {
+  const apiKey = Cypress.env('trelloApiKey');
+  const apiToken = Cypress.env('trelloApiToken');
+  return cy.request({
+    method: 'POST',
+    url: 'https://api.trello.com/1/cards',
+    qs: {
+      name,
+      idList,
+      key: apiKey,
+      token: apiToken
+    }
+  });
+});
+
+Cypress.Commands.add('deleteTrelloCard', (cardId) => {
+  const apiKey = Cypress.env('trelloApiKey');
+  const apiToken = Cypress.env('trelloApiToken');
+  return cy.request({
+    method: 'DELETE',
+    url: `https://api.trello.com/1/cards/${cardId}`,
+    qs: {
+      key: apiKey,
+      token: apiToken
+    }
   });
 });
